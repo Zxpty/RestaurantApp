bab58decc42001ab326a65aec55c455acd63502a
Zxpty <134904618+Zxpty@users.noreply.github.com>
2023-06-15 22:00:26 -0500

    I create new function for in activity_menu because I need to conect with the other class and use new menus.

--- app/src/main/java/com/zxpty/MenuActivity.kt
+++ app/src/main/java/com/zxpty/MenuActivity.kt
@@ -1,13 +1,24 @@
 package com.zxpty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.Zxpty.restaurantapp.R
+import com.zxpty.restaurantapp.MainActivity
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        val btnSeeCart = findViewById<TextView>(R.id.ButtonCarta)
+        btnSeeCart.setOnClickListener { navigateToSeeCart() }
+
+
+    }
+    fun navigateToSeeCart(){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
