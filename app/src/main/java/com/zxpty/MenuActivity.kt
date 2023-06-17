57e089d7ddb5977a7dbbf6bdd7f8592aa2b4842e
Zepty <Anthony-08.2004@hotmail.com>
2023-06-16 21:39:22 -0500

    Change in .idea, I change the textsize of my description and I try to set new function for a full screen

--- app/src/main/java/com/zxpty/MenuActivity.kt
+++ app/src/main/java/com/zxpty/MenuActivity.kt
@@ -1,12 +1,15 @@
 package com.zxpty
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import com.Zxpty.restaurantapp.R
 import com.zxpty.restaurantapp.MainActivity
 
+@Suppress("DEPRECATION")
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +17,28 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
         val btnSeeCart = findViewById<TextView>(R.id.ButtonCarta)
         btnSeeCart.setOnClickListener { navigateToSeeCart() }
-
+        hideSystemUI()
 
     }
     private fun navigateToSeeCart(){
         val intent = Intent(this,MainActivity::class.java)
         startActivity(intent)
+    }
+    private fun hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+        }
+        window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                hideSystemUI()
+            }
+        }
     }
 }
